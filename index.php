1622b6cec23c2417e75db1b1b4d4300be631b20b
Antonio Zakora <antonio.zakora@skole.hr>
2016-03-09 13:36:24 +0100

    Dodali smo index itd

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
 <!DOCTIPE html>
 <html>
 <head>
-        <title>Prvi PHP</title>
+        <title>PRVI PHP</title>
+        <meta charset="utf-8">
 </head>
 
 <body>
-<h1>Prvi PHP</h1>
+<h1>Zaky je najbolji</h1>
 
+<?php
+    echo('Ovo je prije odlomka<br>');
+    echo('<br>');
+    echo('Super, još ćemo dodati<br>');
+
+    echo(date('d.m.Y.'));
+
+    $ocjena = 5;
+    echo('<br>Vaša ocjena je: ' . $ocjena);
+    $nova_var = 166.8;
+    echo('<br>' . $nova_var);
+?>
 <p>
     Ovo je odlomak
 </p>
